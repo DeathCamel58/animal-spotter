e179de87b499efcf727c3d874d00ca6205bf4561
Dylan Corrales <DeathCamel58@gmail.com>
2023-06-01 22:02:11 -0400

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  // Import the global CSS
+  import "../app.css";
+
+  // import 'tailwindcss';
+
+  // Import the header
+  import Header from "../components/header.svelte"
+</script>
+
+<Header />
+
+<slot />
